fcb01ea2dbe59ddd461b83831282094ead15b89d
Ravichandra <ravichandrak89@gmail.com>
2020-12-17 15:14:12 +0530

    lazy load example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Router, RouterModule, Routes } from '@angular/router';
 
+const routes:Routes = [ 
+  {
+    path : '',
+    component : HomeComponent
+  },
+  {
+    path : 'viewdetails',
+    loadChildren: () => import('./viewdetails/viewdetails.module').then(m => m.ViewdetailsModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/viewdetails/viewdetails.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewdetails',
+  template: `
+    <p>
+      viewdetails works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ViewdetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
